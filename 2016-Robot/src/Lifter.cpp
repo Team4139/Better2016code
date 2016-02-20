0a1083da52f10cb9a1de8c9ca758b73315357ea8
zhoudex00 <802minzhu@gmail.com>
2016-02-20 14:32:59 -0800

    Add lifter, loader

--- 2016-Robot/src/Lifter.cpp
+++ 2016-Robot/src/Lifter.cpp
@@ -1,25 +1,42 @@
 #include "WPILib.h"
+//created by Dexin Zhou
 struct Lifter_In
 {
-	float zMovement;
+	//float zMovement;
+
 };
 struct Lifter_Out
 {
-
+double liftamount;
 };
 class Lifter
 {
+	Victor m_lift;
+	Victor m_liftb;
+	Joystick m_stick;
 private:
 
 public:
-	Lifter()
+		int liftport=1;
+		int liftportb=2;
+		int stickport=1;
+
+	Lifter():
+m_lift(liftport),
+m_liftb(liftportb),
+
+m_stick(stickport)
 {
 		// Stuff
 }
 	Lifter_Out Run(Lifter_In input)
 	{
 		Lifter_Out output;
+		output.liftamount=m_stick.GetY();
 
+		m_lift.Set(output.liftamount);
+		m_liftb.Set(output.liftamount);
+		//SmartDashboard::PutNumber("lift",output.liftamount);
 		// Code here
 
 		return output;
--- 2016-Robot/src/Loader.cpp
+++ 2016-Robot/src/Loader.cpp
@@ -1,25 +1,50 @@
 #include "WPILib.h"
+//created by Dexin Zhou
 struct Loader_In
 {
-	float movement;
+	//float movement;
 };
 struct Loader_Out
 {
-
+bool loaddirection;
 };
 class Loader
 {
+	Victor m_loader;
+	Victor m_loaderb;
+	Joystick m_stick;
+
 private:
 
 public:
+	int loadport=1;
+	int loadportb=2;
+	int stickport=1;
+
+	double loadspeed=0.5;
+	int direction=0;
 	Loader()
 {
+		m_loader(loadport),
+		m_loaderb(loadportb),
+		m_stick(stickport);
+
 		//You can sort this out
 }
+
 	Loader_Out Run(Loader_In input)
 	{
 		Loader_Out output;
+		if (m_stick.GetRawButton(2)==1){
+			direction=1;
+		}
+		else if(m_stick.GetButton(2)==1){
+			direction=-1;
+		}
 
+		m_loader.Set(loadspeed*direction);
+		output.loaddirection=direction;
+		//SmartDashboard::PutNumber("load direction",output.loaddirection);
 		// Code here
 
 		return output;
