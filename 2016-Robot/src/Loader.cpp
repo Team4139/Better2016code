ba00cb9d4285015d055bb3488db48182ed09e843
zhoudex00 <802minzhu@gmail.com>
2016-02-23 16:24:03 -0800

    add 3rd motor to loader, edit wheel

--- 2016-Robot/src/Loader.cpp
+++ 2016-Robot/src/Loader.cpp
@@ -11,23 +11,27 @@ class Loader
 {
 	Victor m_loader;
 	Victor m_loaderb;
+	Victor m_loaderc;
 	Joystick m_stick;
 
 private:
 
 public:
-	int loadport=1;
-	int loadportb=2;
-	int stickport=1;
+	uint32_t loadport=0;
+	uint32_t loadportb=7;
+	uint32_t loadportc=9;
+	uint32_t stickport=5;
 	int stickloadinbutton=1;
 	int stickloadoutbutton=2;
-	double loadspeed=0.5;
+	double loadspeed=0.1;
 	Loader()
 {
-		m_loader(loadport),
-		m_loaderb(loadportb),
-		m_stick(stickport);
-
+		m_loader(0),
+		m_loaderb(7),
+		m_loaderc(9),
+		m_stick(5);
+		m_loader.SetInverted(1);//moter are reverted
+		m_loaderc.SetInverted(1);
 		//You can sort this out
 }
 
@@ -45,6 +49,9 @@ public:
 		}
 
 		m_loader.Set(loadspeed*output.loaddirection);
+		m_loaderb.Set(loadspeed*output.loaddirection);
+		m_loaderc.Set(loadspeed*output.loaddirection);
+
 		//SmartDashboard::PutNumber("load direction",output.loaddirection);
 		// Code here
 
--- 2016-Robot/src/Wheels.cpp
+++ 2016-Robot/src/Wheels.cpp
@@ -2,7 +2,7 @@
 
 struct Wheels_In
 {
-	float xMovement, yMovement, rotation, gyroAngle;
+	float yMovement, rotation;
 	int turboMode; // 1: slow, 2: normal, 3: turbo
 };
 struct Wheels_Out
@@ -17,37 +17,16 @@ public:
 	MotorSafetyHelper* driveSafety;
 	Wheels()
 	{
-		drive = new RobotDrive(0,1,2,3); // FrontLeft, FrontRight, RearLeft, RearRight
+		drive = new RobotDrive(4,2,3,1); // FrontLeft, FrontRight, RearLeft, RearRight
 		drive->SetExpiration(0.1);
 		drive->SetSafetyEnabled(true);
 		driveSafety = new MotorSafetyHelper(drive);
-		stick(stickport);
 	}
 	Wheels_Out Run(Wheels_In input)
 	{
 		Wheels_Out output;
-		int mode;
-		if(stick.GetRawButton(1)==1){//tank drive
-			mode=1;
-			Wait(0.5);
-			std::cout<<"mode 1"<<std::endl;
-		}
-		else if(stick.GetRawButton(2)==1){//arcade drive
-			mode=2;
-			Wait(0.5);
-			std::cout<<"mode 2"<<std::endl;
-		}
-
-		float xMovement=input.xMovement*(input.turboMode /2);
-		float yMovement=input.yMovement*(input.turboMode /2);
-		if(mode==1){
-			drive->TankDrive(xMovement,yMovement);
-		}
-		else if(mode==2){
-			drive->ArcadeDrive(yMovement,input.rotation);
-		}
-		// Code here
-
+		drive->ArcadeDrive(input.yMovement,input.rotation);
 		return output;
 	}
+	// Code here
 };
